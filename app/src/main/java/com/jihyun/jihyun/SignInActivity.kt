2f0ea1a694579eb303aeaa5438ca658b060c69d9
jihyunniiii <jita1125@naver.com>
2023-01-18 12:44:02 +0900

    Sign Up View - ID, Password 조건 만족 시 버튼 활성화 기능 구현

--- app/src/main/java/com/jihyun/jihyun/SignInActivity.kt
+++ app/src/main/java/com/jihyun/jihyun/SignInActivity.kt
@@ -1,11 +1,24 @@
 package com.jihyun.jihyun
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.jihyun.jihyun.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var signInBinding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        signInBinding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
+
+        signInBinding.btnSignInSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/jihyun/jihyun/SignUpActivity.kt
+++ app/src/main/java/com/jihyun/jihyun/SignUpActivity.kt
@@ -2,10 +2,77 @@ package com.jihyun.jihyun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.jihyun.jihyun.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var signUpBinding: ActivitySignUpBinding
+    private var idEnabled = false
+    private var passwordEnabled = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        signUpBinding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+
+        signUpBinding.etSignUpId.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                var idStr : String = signUpBinding.etSignUpId.text.toString()
+
+                if (idStr.length >= 6 && idStr.length <= 10) {
+                    idEnabled = true
+
+                    Log.d("SignUp", "id Check!")
+                }
+                else {
+                    idEnabled = false
+                }
+
+                if (idEnabled && passwordEnabled) {
+                    signUpBinding.btnSignUpSignUp.isEnabled = true
+                }
+            }
+        })
+
+        signUpBinding.etSignUpPassword.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                var passwordStr : String = signUpBinding.etSignUpPassword.text.toString()
+
+                if (passwordStr.length >= 8 && passwordStr.length <= 12) {
+                    passwordEnabled = true
+
+                    Log.d("SignUp", "password Check!")
+                }
+                else {
+                    passwordEnabled = false
+                }
+
+                if (idEnabled && passwordEnabled) {
+                    signUpBinding.btnSignUpSignUp.isEnabled = true
+                }
+            }
+        })
+
     }
 }
