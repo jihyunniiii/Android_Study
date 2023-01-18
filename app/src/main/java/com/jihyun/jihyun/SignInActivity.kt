e6d2e722fb1dfa6c85f2f47216d0080c955bbbf9
jihyunniiii <jita1125@naver.com>
2023-01-18 13:28:02 +0900

    Sign Up View - SIGN UP 버튼 클릭 시 입력 정보 넘기고 SignInActivity로 이동

--- app/src/main/java/com/jihyun/jihyun/SignInActivity.kt
+++ app/src/main/java/com/jihyun/jihyun/SignInActivity.kt
@@ -3,22 +3,32 @@ package com.jihyun.jihyun
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.jihyun.jihyun.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var signInBinding: ActivitySignInBinding
+    private var savedID = ""
+    private var savedPassword = ""
+    private var savedMBTI = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        savedID = intent.getStringExtra("ID").toString()
+        savedPassword = intent.getStringExtra("Password").toString()
+        savedMBTI = intent.getStringExtra("MBTI").toString()
+
+        Log.d("checkID", savedID + " / " + savedPassword + " / " + savedMBTI)
+
         signInBinding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
 
         signInBinding.btnSignInSignUp.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+            val signInIntent = Intent(this, SignUpActivity::class.java)
+            startActivity(signInIntent)
         }
     }
 }
--- app/src/main/java/com/jihyun/jihyun/SignUpActivity.kt
+++ app/src/main/java/com/jihyun/jihyun/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.jihyun.jihyun
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -20,7 +21,7 @@ class SignUpActivity : AppCompatActivity() {
 
         signUpBinding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
 
-        signUpBinding.etSignUpId.addTextChangedListener(object: TextWatcher {
+        signUpBinding.etSignUpId.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
             }
@@ -30,14 +31,13 @@ class SignUpActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(p0: Editable?) {
-                var idStr : String = signUpBinding.etSignUpId.text.toString()
+                var idStr: String = signUpBinding.etSignUpId.text.toString()
 
                 if (idStr.length >= 6 && idStr.length <= 10) {
                     idEnabled = true
 
                     Log.d("SignUp", "id Check!")
-                }
-                else {
+                } else {
                     idEnabled = false
                 }
 
@@ -47,7 +47,7 @@ class SignUpActivity : AppCompatActivity() {
             }
         })
 
-        signUpBinding.etSignUpPassword.addTextChangedListener(object: TextWatcher {
+        signUpBinding.etSignUpPassword.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
             }
@@ -57,14 +57,13 @@ class SignUpActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(p0: Editable?) {
-                var passwordStr : String = signUpBinding.etSignUpPassword.text.toString()
+                var passwordStr: String = signUpBinding.etSignUpPassword.text.toString()
 
                 if (passwordStr.length >= 8 && passwordStr.length <= 12) {
                     passwordEnabled = true
 
                     Log.d("SignUp", "password Check!")
-                }
-                else {
+                } else {
                     passwordEnabled = false
                 }
 
@@ -73,6 +72,14 @@ class SignUpActivity : AppCompatActivity() {
                 }
             }
         })
+
+        signUpBinding.btnSignUpSignUp.setOnClickListener {
+            val signUpIntent = Intent(this, SignInActivity::class.java)
+            signUpIntent.putExtra("ID", signUpBinding.etSignUpId.text.toString())
+            signUpIntent.putExtra("Password", signUpBinding.etSignUpPassword.text.toString())
+            signUpIntent.putExtra("MBTI", signUpBinding.etSignUpMbti.text.toString())
+            startActivity(signUpIntent)
+        }
 
     }
 }
